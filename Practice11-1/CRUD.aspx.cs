2e9bcc42deb70c20910e2e68a84dd2de3f19aa8c
昱瑋 李 <107111226@mail.oit.edu.tw>
2021-06-21 15:56:48 +0800

    11-1

--- Practice11-1/CRUD.aspx.cs
+++ Practice11-1/CRUD.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Practice11_1
 {
@@ -11,6 +13,26 @@ namespace Practice11_1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = test; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False
+            string s = "";
+            s = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=test;" +
+                "Integrated Security=True;Connect Timeout=30;Encrypt=False;" +
+                "TrustServerCertificate=False;ApplicationIntent=ReadWrite;" +
+                "MultiSubnetFailover=False;" +
+                "User ID=sa;Password=123456";
+            SqlConnection o_connect = new SqlConnection(s);
+            try
+            {
+                SqlCommand o_com = new SqlCommand("update Users set Birthday='2021/5/18' where Name=N'狗狗旭'", o_connect);
+                o_connect.Open();
+                o_com.ExecuteNonQuery();
+                o_connect.Close();
+            }
+            catch (Exception error)
+            {
+                Response.Write(error.ToString());
+            }
+
 
         }
     }
